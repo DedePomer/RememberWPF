35bb615996637b953f08074a55197bb5ff9c0bbd
=DedePomer <=t343st@gmail.com>
2024-03-20 13:08:12 +0300

    Написал главный класс

--- Infostruction/Commands/RelateC.cs
+++ Infostruction/Commands/RelateC.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RememberWPF.Infostruction.Commands.Base;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,23 @@ using System.Threading.Tasks;
 
 namespace RememberWPF.Infostruction.Commands
 {
-    class RelateC
+    internal class RelateC : CommandBase
     {
+        private Action<object> _Execute;
+        private Func<object?, bool> _CanExecute;
+
+        public RelateC(Action<object> Execute, Func<object, bool> Canexecute = null) 
+        {
+            _Execute = Execute ?? throw new ArgumentNullException(nameof(Execute));
+            _CanExecute = CanExecute;
+        }
+
+        public override bool CanExecute(object? parameter) => _CanExecute?.Invoke(parameter) ?? true;
+
+
+        public override void Execute(object? parameter)
+        {
+            _Execute(parameter);
+        }
     }
 }
