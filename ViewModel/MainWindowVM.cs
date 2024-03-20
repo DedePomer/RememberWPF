734d8c193d54b7feec23bec1f08ee1a9a92019ba
=DedePomer <=t343st@gmail.com>
2024-03-20 13:23:54 +0300

    Добавил команду прекращение работы ппрриложения

--- ViewModel/MainWindowVM.cs
+++ ViewModel/MainWindowVM.cs
@@ -1,22 +1,51 @@
-﻿using RememberWPF.ViewModel.Base;
+﻿using RememberWPF.Infostruction.Commands;
+using RememberWPF.ViewModel.Base;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace RememberWPF.ViewModel
 {
     class MainWindowVM : ViewModelBase
     {
-        private int _Click;
-        public int Click
-        {  get { return _Click; }
-           set 
-           {
-               _Click = value;
-               OnPropertyChanged();
-           }
+        #region Команды
+
+        #region CloseAppCommand
+        public ICommand CloseAppCommand { get; }
+
+        private bool CanCloseAppCommandExecute(object p) => true;
+        private void OnCloseAppCommandExecuted(object p) 
+        {
+            Application.Current.Shutdown();
         }
+        #endregion
+
+
+
+
+        #endregion
+
+
+        public MainWindowVM()
+        {
+            #region Комманды
+            CloseAppCommand = new RelateC(OnCloseAppCommandExecuted, CanCloseAppCommandExecute);
+
+            #endregion
+        }
+
+        //private int _Click;
+        //public int Click
+        //{  get { return _Click; }
+        //   set 
+        //   {
+        //       _Click = value;
+        //       OnPropertyChanged();
+        //   }
+        //}
     }
 }
