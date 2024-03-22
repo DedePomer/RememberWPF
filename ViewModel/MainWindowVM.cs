86ad92089c835ae7e0219a80e72ce22419a7937d
=DedePomer <=t343st@gmail.com>
2024-03-22 20:24:10 +0300

    Изменил окошко со скримером и добивил поля для него же

--- ViewModel/MainWindowVM.cs
+++ ViewModel/MainWindowVM.cs
@@ -10,20 +10,42 @@ using System.Windows.Input;
 
 namespace RememberWPF.ViewModel
 {
-    class MainWindowVM : ViewModelBase
+    internal class MainWindowVM : ViewModelBase
     {
+        #region Число в Скримере
+        private int _ScreamNum = 0;           
+        public int ScreamNum
+        { 
+            get { return _ScreamNum; }
+            set 
+            {
+                if (Equals(_ScreamNum, value)) return;
+                _ScreamNum = value;
+                OnPropertyChanged();
+            }
+        }
+        #endregion
+
+
+
         #region Команды
 
         #region CloseAppCommand
         public ICommand CloseAppCommand { get; }
-
         private bool CanCloseAppCommandExecute(object p) => true;
         private void OnCloseAppCommandExecuted(object p) 
         {
             Application.Current.Shutdown();
         }
         #endregion
-
+        #region ClickScreamCommand
+        public ICommand ClickScreamCommand { get; }
+        private bool CanClickScreamCommandExecute(object p) => true;
+        private void OnClickScreamCommandExecuted(object p)
+        {
+            if (ScreamNum<11) ScreamNum += 1;
+        }
+        #endregion
 
 
 
